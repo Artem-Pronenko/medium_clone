22febdd7e702149b68d6eee7aa3b86312dd2d309
Artem-Pronenko <pronenko.artem@gmail.com>
2020-12-14 12:43:29 +0200

    added auth firebase

--- src/index.js
+++ src/index.js
@@ -5,6 +5,21 @@ import Routes from 'routes'
 import TopBar from 'components/TopBar'
 import {CurrentUserProvider} from 'contexts/currentUser'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDo0BwzikoCxLLmNUcR43a5xK2iscMtq9Q',
+  authDomain: 'react-medium-clone-ap.firebaseapp.com',
+  projectId: 'react-medium-clone-ap',
+  storageBucket: 'react-medium-clone-ap.appspot.com',
+  messagingSenderId: '504759534434',
+  appId: '1:504759534434:web:65bd6bd5762ca00428623b'
+}
+
+firebase.initializeApp(firebaseConfig)
+
 const App = () => {
   return (
     <CurrentUserProvider>
